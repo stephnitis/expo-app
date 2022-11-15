8dc8f4659414720faecfe6cbb068dddd77094a4d
Stephanie Hill <stephni.hill@gmail.com>
2022-11-15 10:05:50 -0800

    workin stickers

--- Components/EmojiList.js
+++ Components/EmojiList.js
@@ -24,7 +24,7 @@ export default function EmojiList({ onSelect, onCloseModal }) {
               onSelect(item);
               onCloseModal();
             }}>
-              <Image source={image} key={index} style={styles.image} />
+              <Image source={item} key={index} style={styles.image} />
           </Pressable>
         );
       }}
